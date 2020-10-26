1cc02a13db6e33fa7b8ddaf6741d0f049deff2c1
Slowpokita <gricel>
2020-10-26 20:34:17 -0300

    fix agus 1

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM, { render } from 'react-dom'
 import Home from "./views/Home"
 
 
-ReactDOM.render(<Home config="wena en development"/>, document.getElementById('root'))
+//ReactDOM.render(<Home config="wena en development"/>, document.getElementById('root'))
 
 export const init = (config:string) => {
   ReactDOM.render(<Home config={config} />, document.getElementById('root'));
